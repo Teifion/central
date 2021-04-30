c9563ef7b45aa9cb615451cc009904f7d84e5282
Teifion <teifion@teifion.co.uk>
2021-04-30 10:33:44 +0100

    Fixed some tests

--- lib/central_web/controllers/account/registration_controller.ex
+++ lib/central_web/controllers/account/registration_controller.ex
@@ -72,7 +72,7 @@ defmodule CentralWeb.Account.RegistrationController do
     user = Account.get_user!(conn.user_id)
 
     case Account.update_user(user, user_params, :password) do
-      {:ok, user} ->
+      {:ok, _user} ->
         # User password updated
 
         conn
